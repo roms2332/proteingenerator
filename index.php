51afae4fd87396e249defd8b19d45989e6c5173b
root <root@raspberrypi.(none)>
2015-04-16 19:20:40 +0000

    Index

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
-<p>Crée ta protéine :D<br/>
-Les lettres acceptées sont A, T, C, et G.</p>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RomProt</title>
+    </head>
+    <body>
+        <h1 class="text-center">RomPort</h1>
 
-<form method="post">
-	<input name="seq" type="text"/>
-	<input action="" type="submit"/>
-</form>
+        <p>Crée ta protéine :D<br/>
+        Les lettres acceptées sont A, T, C, et G.</p>
 
-<?
+        <form method="post" action="#">
+            <input type="text" name="seq" placeholder="Séquence" autofocus required />
+            <input type="submit" value="Envoyer" />
+        </form>
+    </body>
+</html>
 
-$test = strpos($_POST['seq'], "Z, E, R");
-if($test === false){
-	$seq = $_POST['seq'];
-	$final = strlen($seq);
-	echo $final;
+<?php
+echo $_POST['seq'] .' : ';
+
+if (preg_match('/^[t|a|c|g]*$/', strtolower($_POST['seq']))) {
+	$aa = floor(strlen($_POST['seq']) / 3);
+	echo "Valid sequence, ".$aa." amino-acids. ";
+} else {
+    echo "Invalid sequence";
 }
-else { echo "Seulement ATCG"; }
 ?>
-
